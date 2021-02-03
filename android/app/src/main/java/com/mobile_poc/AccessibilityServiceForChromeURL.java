b588e3c6065cefab983febfa081fb534480469a9
felix <lambertfelix8@gmail.com>
2021-02-03 13:05:58 +0100

    feat: add non connected to react-native accessibility service

---
+++ android/app/src/main/java/com/mobile_poc/AccessibilityServiceForChromeURL.java
@@ -0,0 +1,18 @@
+package com.mobile_poc;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class AccessibilityServiceForChromeURL extends AccessibilityService {
+
+  @Override
+  public void onAccessibilityEvent(AccessibilityEvent event) {
+
+  }
+
+  @Override
+  public void onInterrupt() {
+
+  }
+
+}
