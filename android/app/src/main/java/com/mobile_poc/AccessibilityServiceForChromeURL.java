48ac03e006410e20a6b039fc444769255d27135d
felix <lambertfelix8@gmail.com>
2021-02-03 20:04:10 +0100

    feat: read URL events from chrome

--- android/app/src/main/java/com/mobile_poc/AccessibilityServiceForChromeURL.java
+++ android/app/src/main/java/com/mobile_poc/AccessibilityServiceForChromeURL.java
@@ -2,9 +2,12 @@ package com.mobile_poc;
 
 import com.mobile_poc.AccessibilityServiceForChromeURLModule;
 
+import java.util.List;
+
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 public class AccessibilityServiceForChromeURL extends AccessibilityService {
 
@@ -23,14 +26,39 @@ public class AccessibilityServiceForChromeURL extends AccessibilityService {
     AccessibilityServiceForChromeURLModule.prepareEvent("Accessibility service has been deactivated by user");
   }
 
+  private String captureUrl(AccessibilityNodeInfo info) {
+    List<AccessibilityNodeInfo> nodes = info.findAccessibilityNodeInfosByViewId("com.android.chrome:id/url_bar");
+    if (nodes == null || nodes.size() <= 0) {
+      return null;
+    }
+    AccessibilityNodeInfo addressBarNodeInfo = nodes.get(0);
+    
+    String url = null;
+    if (addressBarNodeInfo.getText() == null) {
+      return null;
+    }
+    url = addressBarNodeInfo.getText().toString();
+    addressBarNodeInfo.recycle();
+    return url;
+  }
+
   @Override
   public void onAccessibilityEvent(AccessibilityEvent event) {
-    AccessibilityServiceForChromeURLModule.prepareEvent("The user is doing something with the chrome app");
+    AccessibilityNodeInfo parentNodeInfo = event.getSource();
+    if (parentNodeInfo == null) {
+      return;
+    }
+
+    String capturedUrl = captureUrl(parentNodeInfo);
+    if (capturedUrl == null) {
+      return;
+    }
+
+    AccessibilityServiceForChromeURLModule.prepareEvent(capturedUrl);
   }
 
   @Override
   public void onInterrupt() {
-
+    AccessibilityServiceForChromeURLModule.prepareEvent("on interrupt");
   }
-
 }
