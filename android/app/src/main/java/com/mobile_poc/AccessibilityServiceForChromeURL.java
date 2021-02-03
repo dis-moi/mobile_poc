85fbcdd0b4b8f9dc5bc22a713ab3e64f2ee1d7f8
felix <lambertfelix8@gmail.com>
2021-02-03 15:58:36 +0100

    feat: when user activates or deactivates accessibility an event is sent to react native

--- android/app/src/main/java/com/mobile_poc/AccessibilityServiceForChromeURL.java
+++ android/app/src/main/java/com/mobile_poc/AccessibilityServiceForChromeURL.java
@@ -1,9 +1,21 @@
 package com.mobile_poc;
 
+import com.mobile_poc.AccessibilityServiceForChromeURLModule;
+
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 
 public class AccessibilityServiceForChromeURL extends AccessibilityService {
+
+  @Override
+  protected void onServiceConnected() {
+    AccessibilityServiceForChromeURLModule.prepareEvent("Accessibility service has been activated by user");
+  }
+
+  @Override
+  public void onDestroy() {
+    AccessibilityServiceForChromeURLModule.prepareEvent("Accessibility service has been deactivated by user");
+  }
 
   @Override
   public void onAccessibilityEvent(AccessibilityEvent event) {
