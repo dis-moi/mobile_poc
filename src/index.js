29ba7a0d2d82a4082bebe7526f54b34f69c76830
Felix Lambert <lambertfelixpro@gmail.com>
2021-06-03 12:29:27 +0900

    feat: do welcome page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useNativeModuleEffects from './useNativeModuleEffects';
-import { TouchableOpacity, View, Image, Linking, Button } from 'react-native';
+import { Container, Content } from 'native-base';
+import { TouchableOpacity, View, Linking, Button, Text } from 'react-native';
 
 import { Background } from './nativeModules/get';
 
@@ -15,15 +16,50 @@ function App() {
   }, []);
 
   return (
-    <View style={{ flex: 1, flexDirection: 'column' }}>
-      <TouchableOpacity
-        onPress={() => {
-          Linking.openURL('https://www.google.com');
+    <Container>
+      <Content
+        padder
+        contentContainerStyle={{
+          flex: 1,
         }}
       >
-        <Image source={require('./images/demo.png')} />
-      </TouchableOpacity>
-    </View>
+        <Text style={{ textAlign: 'center' }}>DISMOI</Text>
+        <View
+          style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}
+        >
+          <Text style={{ textAlign: 'center' }}>
+            Merci d'avoir téléchargé l'application DisMoi. {'\n'}
+          </Text>
+          <Text style={{ textAlign: 'center' }}>
+            Conseils et entraide directement sur les pages web que vous visitez.
+            {'\n'}
+          </Text>
+          <Text style={{ textAlign: 'center' }}>
+            S'il existe une meilleure alternative, une info éclairante, vous le
+            saurez! {'\n'}
+          </Text>
+          <Text style={{ textAlign: 'center' }}>
+            Gratuit, sans publicité, respecte votre vie privée et ne ralentit
+            pas votre mobile {'\n'}
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL('https://www.dismoi.io/cgu/');
+            }}
+          >
+            <Text style={{ textAlign: 'center' }}>CGU?</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Button title="Suivant" />
+        </View>
+      </Content>
+    </Container>
   );
 }
 
