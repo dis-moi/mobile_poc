c70e81a6396d9a173b0a91c2853ceea8f2fff065
Felix Lambert <lambertfelixpro@gmail.com>
2021-07-12 14:11:53 +0200

    bug: bubble disappear when user taps screen

--- android/app/src/main/java/com/dismoi/scout/accessibility/BackgroundService.kt
+++ android/app/src/main/java/com/dismoi/scout/accessibility/BackgroundService.kt
@@ -1,9 +1,6 @@
 package com.dismoi.scout.accessibility
 
 import android.accessibilityservice.AccessibilityService
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -24,7 +21,7 @@ class BackgroundService : AccessibilityService() {
   private var _eventText: String? = ""
   private var _hide: String? = ""
 
-  private val NOTIFICATION_TIMEOUT: Long = 200
+  private val NOTIFICATION_TIMEOUT: Long = 300
 
   private val handler = Handler()
   private val runnableCode: Runnable = object : Runnable {
@@ -39,6 +36,8 @@ class BackgroundService : AccessibilityService() {
       bundle.putString("packageName", _packageName)
       bundle.putString("eventText", _eventText)
       bundle.putString("hide", _hide)
+
+      _hide = ""
 
       myIntent.putExtras(bundle)
 
@@ -61,17 +60,26 @@ class BackgroundService : AccessibilityService() {
 
   @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
   private fun captureUrl(info: AccessibilityNodeInfo, config: SupportedBrowserConfig): String? {
-    val nodes = info.findAccessibilityNodeInfosByViewId(config.addressBarId)
-    if (nodes == null || nodes.size <= 0) {
-      return null
+    if (info.childCount > 0) {
+      if (info.getChild(0).className.toString() == "android.widget.LinearLayout") {
+        if (info.childCount > 1) {
+          if (info.getChild(1).className.toString() != "android.widget.EditText") {
+            val nodes = info.findAccessibilityNodeInfosByViewId(config.addressBarId)
+            if (nodes == null || nodes.size <= 0) {
+              return null
+            }
+            val addressBarNodeInfo = nodes[0]
+            var url: String? = null
+            if (addressBarNodeInfo.text != null) {
+              url = addressBarNodeInfo.text.toString()
+            }
+            addressBarNodeInfo.recycle()
+            return url
+          }
+        }
+      }
     }
-    val addressBarNodeInfo = nodes[0]
-    var url: String? = null
-    if (addressBarNodeInfo.text != null) {
-      url = addressBarNodeInfo.text.toString()
-    }
-    addressBarNodeInfo.recycle()
-    return url
+    return null
   }
 
   private fun getEventType(event: AccessibilityEvent): String? {
@@ -100,95 +108,81 @@ class BackgroundService : AccessibilityService() {
     return "com.android.systemui" == packageName || "com.android.launcher3" == packageName
   }
 
-  fun theseEventsNeedToHideTheBubble(event: AccessibilityEvent): Boolean {
-    return getEventType(event) === "TYPE_VIEW_CLICKED" &&
-      event.getClassName() === "android.widget.FrameLayout" ||
-      getEventType(event) === "TYPE_VIEW_CLICKED" &&
-      event.getPackageName() == "com.android.systemui" ||
-      getEventType(event) === "TYPE_VIEW_TEXT_SELECTION_CHANGED" &&
-      event.getPackageName() == "com.android.chrome" ||
-      getEventType(event) === "TYPE_VIEW_CLICKED" &&
-      event.getPackageName() == "com.android.chrome"
-  }
-
   @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
   override fun onAccessibilityEvent(event: AccessibilityEvent) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       if (canDrawOverlays(applicationContext)) {
-        if (theseEventsNeedToHideTheBubble(event)) {
-          _hide = "true"
-          _url = ""
-          _eventType = getEventType(event)
-          _className = event.getClassName().toString()
-          _packageName = event.getPackageName().toString()
-          _eventText = getEventText(event)
-          handler.post(runnableCode)
-          return
-        }
+        if (
+          event.getClassName() != null && 
+          event!!.getPackageName() != null && 
+          !isLauncherPackage(event!!.getPackageName())
+        ) {
+          if (event.getClassName().toString() == "com.android.inputmethod.keyboard.Key") {
+            _hide = "true"
+            _url = ""
+            _eventType = getEventType(event)
+            _className = event.getClassName().toString()
+            _packageName = event.getPackageName().toString()
+            _eventText = getEventText(event)
+            handler.post(runnableCode)
+            return
+          }
+          val parentNodeInfo = event.source ?: return
+          val packageName = event.packageName.toString()
+          var browserConfig: SupportedBrowserConfig? = null
 
-        if (event!!.getPackageName() != null) {
-          if (!isLauncherPackage(event!!.getPackageName())) {
-            val parentNodeInfo = event.source ?: return
-            val packageName = event.packageName.toString()
-            var browserConfig: SupportedBrowserConfig? = null
-
-
-            for (supportedConfig in getSupportedBrowsers()) {
-              if (supportedConfig.packageName == packageName) {
-                browserConfig = supportedConfig
-              }
+          for (supportedConfig in getSupportedBrowsers()) {
+            if (supportedConfig.packageName == packageName) {
+              browserConfig = supportedConfig
             }
+          }
 
-            // this is not supported browser, so exit
-            if (browserConfig == null) {
-              return
-            }
-            val capturedUrl = captureUrl(parentNodeInfo, browserConfig)
-            parentNodeInfo.recycle()
+          // this is not supported browser, so exit
+          if (browserConfig == null) {
+            return
+          }
 
-            // we can't find an url. Browser either was updated or opened page without url text field
-            if (capturedUrl == null) {
-              return
-            }
+          val nodeInfo = event.source
 
-            val eventTime = event.eventTime
-            val detectionId = "$packageName"
-            val lastRecordedTime =
-              if (previousUrlDetections.containsKey(detectionId)) {
-                previousUrlDetections[detectionId]!!
-              } else 0.toLong()
+          val capturedUrl = captureUrl(nodeInfo, browserConfig)
 
-            // some kind of redirect throttling
-            if (eventTime - lastRecordedTime > NOTIFICATION_TIMEOUT) {
-              Log.d("Notification", "POST WITH URL")
-              previousUrlDetections[detectionId] = eventTime
+          parentNodeInfo.recycle()
 
+          // we can't find an url. Browser either was updated or opened page without url text field
+          if (capturedUrl == null ) {
+            return
+          }
+
+          val eventTime = event.eventTime
+          val detectionId = "$packageName"
+          val lastRecordedTime =
+            if (previousUrlDetections.containsKey(detectionId)) {
+              previousUrlDetections[detectionId]!!
+            } else 0.toLong()
+
+          // some kind of redirect throttling
+          if (eventTime - lastRecordedTime > NOTIFICATION_TIMEOUT) {
+
+            previousUrlDetections[detectionId] = eventTime
+
+            if (_url != capturedUrl) {
               _url = capturedUrl
               _eventType = getEventType(event)
               _className = event.getClassName().toString()
               _packageName = event.getPackageName().toString()
               _eventText = getEventText(event)
-              _hide = "false"
+              //_hide = "false"
+
               handler.post(runnableCode)
             }
           }
-
         }
-
       }
     }
   }
 
   override fun onInterrupt() {
     sendEventFromAccessibilityServicePermission("false");
-  }
-
-  private fun packageNames(): Array<String> {
-    val packageNames: MutableList<String> = ArrayList()
-    for (config in getSupportedBrowsers()) {
-      packageNames.add(config.packageName)
-    }
-    return packageNames.toTypedArray()
   }
 
   private class SupportedBrowserConfig(var packageName: String, var addressBarId: String)
